cae1e78e5643c7b13e0153d56b0da8244b21f146
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-04-06 10:45:14 +0300

    add data to products page

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -3,24 +3,62 @@ import { useDispatch } from 'react-redux';
 
 import { instance } from '@/axios/axiosDefault';
 import { getProductsByCategory } from '@/redux/products/slice';
+import Image from 'next/image';
 
 export async function getServerSideProps({ query }) {
   const category = query.category;
-  const response = await instance(`/products/list?group_id=${category}`);
-  const data = response.data;
+
+  const response = await instance(`/products/${category ? `list?limit=500&group_id=${category}` : 'list?limit=500'}`);
+  // const response = await instance(`/products/list`);
+  const data = response.data.products;
 
   return {
-    props: { data },
+    props: { data, query },
   };
 }
 
-function Products({ data }) {
+function Products({ data, query }) {
+  console.log(query);
+  console.log(data);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProductsByCategory(data));
   }, [data, dispatch]);
 
-  return <div>Products</div>;
+  return (
+    <ul
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '10px',
+        alignItems: 'start',
+        padding: '20px',
+        justifyContent: 'center',
+      }}
+    >
+      {data?.map((item, idx) => (
+        <li
+          key={item.id}
+          style={{
+            display: 'flex',
+            gap: '10px',
+            border: '1px solid grey',
+            width: '300px',
+            alignItems: 'center',
+            padding: '10px',
+          }}
+        >
+          <Image
+            src={item.main_image}
+            alt="fotka"
+            width={150}
+            height={150}
+          />
+          <p>{`${idx + 1} :${item.name}`}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Products;
