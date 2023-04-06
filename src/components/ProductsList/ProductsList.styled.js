dc76a3f1f4d4421ab8027730f96e600d2ff333c3
linluxx <karolina.kkv@gmail.com>
2023-04-07 01:09:19 +0300

    Update ProductsList.styled.js

--- src/components/ProductsList/ProductsList.styled.js
+++ src/components/ProductsList/ProductsList.styled.js
@@ -6,16 +6,18 @@ export const List = styled.ul`
 `;
 
 export const Item = styled.li`
-  width:310px;
+  width: 310px;
   border: 1px solid ${(p) => p.theme.colors.border};
   border-radius: 20px;
-  margin-right:27px;
-  margin-bottom:24px;
+  margin-right: 27px;
+  margin-bottom: 24px;
 
-transition: transform ${(p) => p.theme.transition}, box-shadow ${(p) => p.theme.transition};
+  transition: transform ${(p) => p.theme.transition}, box-shadow ${(p) => p.theme.transition};
 
   &:hover,
   &:focus {
     transform: scale(1.05);
     box-shadow: 0px 3px 26px -1px ${(p) => p.theme.colors.darkHover};
-    outline: transparent;`;
+    outline: transparent;
+  }
+`;
