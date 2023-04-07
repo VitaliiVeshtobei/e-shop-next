3b4ed6c766725154b7700e7c855dccda62b5f245
linluxx <karolina.kkv@gmail.com>
2023-04-07 21:25:51 +0300

    added pagination

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,17 +1,33 @@
+import { useState } from 'react';
+
 import { useSelector } from 'react-redux';
 
 import ProductCard from './ProductCard/ProductCard';
 import { selectProductsByCategory } from '@/redux/products/selectors';
 
-import { List, Item } from './ProductsList.styled';
+import { List, Item, Pagination } from './ProductsList.styled';
 
 const ProductsList = () => {
+  const [itemOffset, setItemOffset] = useState(0);
+
   const products = useSelector(selectProductsByCategory);
+  const itemsPerPage = 20;
+
+  const endOffset = itemOffset + itemsPerPage;
+
+  const currentItems = products.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(products.length / itemsPerPage);
+  console.log(pageCount);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    setItemOffset(newOffset);
+  };
 
   return (
     <>
       <List>
-        {products?.map((item) => (
+        {currentItems?.map((item) => (
           <Item key={item.id}>
             <ProductCard
               id={item.id}
@@ -25,6 +41,18 @@ const ProductsList = () => {
           </Item>
         ))}
       </List>
+      {pageCount > 1 && (
+        <Pagination
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+          className="Pagination"
+        />
+      )}
     </>
   );
 };
