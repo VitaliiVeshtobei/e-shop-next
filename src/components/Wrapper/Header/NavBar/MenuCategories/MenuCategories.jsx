28837249d8873a58188c7a865f48abd40e5355f5
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-04-24 15:17:21 +0300

    fix touch scroll

--- src/components/Wrapper/Header/NavBar/MenuCategories/MenuCategories.jsx
+++ src/components/Wrapper/Header/NavBar/MenuCategories/MenuCategories.jsx
@@ -5,16 +5,20 @@ const MenuCategories = ({ handleClick, data, nameButton }) => {
   useEffect(() => {
     window.addEventListener('wheel', noScroll, { passive: false });
     window.addEventListener('keydown', onKeyDown);
+    document.addEventListener('touchmove', noScroll, { passive: false });
 
     return () => {
       window.removeEventListener('keydown', onKeyDown);
       window.removeEventListener('wheel', noScroll);
+      document.removeEventListener('touchmove', noScroll);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   const noScroll = (e) => {
     e.preventDefault();
   };
+
   const onKeyDown = (e) => {
     if (e.code === 'Escape') {
       handleClick();
