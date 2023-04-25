6eb07b7282ed259bcd6dc8937c8ac710e74a2d00
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-04-25 10:23:30 +0300

    fix eslint

--- src/components/DeliveryAndPayment/DeliveryAndPayment.jsx
+++ src/components/DeliveryAndPayment/DeliveryAndPayment.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { ListItem, ListText, Title } from './DeliveryAndPayment.styled';
 import { TbTruckDelivery } from 'react-icons/tb';
