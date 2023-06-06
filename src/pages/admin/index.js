4fdc44361b7fe35912ae6412421c61f099889b45
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-06-06 13:31:27 +0300

    finish logic admin

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,18 +1,18 @@
-import { useRouter } from 'next/router';
+import { wrapper } from '@/redux/store';
 
 const Admin = () => {
-  const router = useRouter();
-
   return null; // Порожній компонент, оскільки ми виконаємо редірект на сервері
 };
 
-export async function getServerSideProps() {
+export const getServerSideProps = wrapper.getServerSideProps((store) => ({ req, res, ...etc }) => {
+  const role = store.getState().user.role;
+
   return {
     redirect: {
-      destination: '/admin/admin-panel',
+      destination: `${role !== 'ADMIN' ? '/' : '/admin/admin-panel'}`,
       permanent: false, // Зробити редірект тимчасовим (302)
     },
   };
-}
+});
 
 export default Admin;
