01ec94a2692337aaeb605b1141238ba569be5976
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-06-06 15:42:28 +0300

    fix interceptors

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,18 +1,14 @@
-import { wrapper } from '@/redux/store';
-
 const Admin = () => {
   return null; // Порожній компонент, оскільки ми виконаємо редірект на сервері
 };
 
-export const getServerSideProps = wrapper.getServerSideProps((store) => ({ req, res, ...etc }) => {
-  const role = store.getState().user.role;
-
+export const getServerSideProps = () => {
   return {
     redirect: {
-      destination: `${role !== 'ADMIN' ? '/' : '/admin/admin-panel'}`,
+      destination: '/admin/admin-panel',
       permanent: false, // Зробити редірект тимчасовим (302)
     },
   };
-});
+};
 
 export default Admin;
